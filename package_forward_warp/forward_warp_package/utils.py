8b894f98cd3ad2de917b0586fe88ae7231f2b70e
Lingzhe Zhao <zhaolingzhe@westlake.edu.cn>
2023-05-23 18:34:36 +0800

    fix compile; add docker env

--- package_forward_warp/forward_warp_package/utils.py
+++ package_forward_warp/forward_warp_package/utils.py
@@ -3,10 +3,10 @@ import meshzoo
 
 def generate_2D_mesh(H, W):
     _, faces = meshzoo.rectangle(
-        xmin = -1., xmax = 1.,
-        ymin = -1., ymax = 1.,
-        nx = W, ny = H,
-        zigzag=True)
+        x0 = -1., x1 = 1.,
+        y0 = -1., y1 = 1.,
+        nx = W, ny = H
+    )
 
     x = torch.arange(0, W, 1).float().cuda() 
     y = torch.arange(0, H, 1).float().cuda()
